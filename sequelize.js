528c9a4fad627684c0a3a931b394f7b2fac2b7ea
Palash Bauri <bauripalash@users.noreply.github.com>
2018-12-05 12:27:46 +0530

    Update sequelize.js

--- sequelize.js
+++ sequelize.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
-  dialect: 'mysql',
+  dialect: 'postgres',
   pool: {
     max: 10,
     min: 0,
